4c898d7d594cef42415544384b3a0c254f53eb86
Don Wood <donwood@gmail.com>
2021-03-04 01:18:05 -0600

    Updated allowed hosts to include IP.

--- RPiDjangoSite/settings.py
+++ RPiDjangoSite/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env.str('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['galileo', 'localhost']
+ALLOWED_HOSTS = ['galileo', '192.168.11.10', 'localhost']
 
 
 # Application definition
